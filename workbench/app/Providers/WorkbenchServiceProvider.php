4427f1c869c62ffd0cd6162be29465bd8331fb2e
Durable Workflow <support@durable-workflow.com>
2026-03-24 00:19:28 +0000

    Refocus Copilot setup on agent environment

--- workbench/app/Providers/WorkbenchServiceProvider.php
+++ workbench/app/Providers/WorkbenchServiceProvider.php
@@ -18,7 +18,6 @@ class WorkbenchServiceProvider extends ServiceProvider
                 \Workbench\App\Console\CreateTestWorkflow::class,
                 \Workbench\App\Console\CreateTestParentWorkflow::class,
                 \Workbench\App\Console\CreateTestContinueAsNewWorkflow::class,
-                \Workbench\App\Console\SeedDashboardFixtures::class,
             ]);
         }
     }
